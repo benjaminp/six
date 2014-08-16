72cc15e87ca8d54f0a5d37204994cc4fa9eac2ad
Benjamin Peterson <benjamin@python.org>
2014-08-16 14:32:35 -0700

    use absolute imports (fixes #89)

--- six.py
+++ six.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from __future__ import absolute_import
+
 import functools
 import operator
 import sys
