0004c7e4e2fd777073ad196415f973dbb0912da2
Joshua Harlow <harlowja@gmail.com>
2014-10-11 15:18:45 -0700

    Fix simulatenous delattr
    When two or more threads enter the descriptor one of them
    will delete the attribute and the others will not be able
    to, we should handle that case more gracefully.

--- six.py
+++ six.py
@@ -88,8 +88,12 @@ class _LazyDescr(object):
     def __get__(self, obj, tp):
         result = self._resolve()
         setattr(obj, self.name, result) # Invokes __set__.
-        # This is a bit ugly, but it avoids running this again.
-        delattr(obj.__class__, self.name)
+        try:
+            # This is a bit ugly, but it avoids running this again by
+            # removing this descriptor.
+            delattr(obj.__class__, self.name)
+        except AttributeError:
+            pass
         return result
 
 
