5efb522b0647f7467248273ec1b893d06b984a59
Benjamin Peterson <benjamin@python.org>
2015-01-02 10:34:20 -0600

    six 1.9.0

--- six.py
+++ six.py
@@ -29,7 +29,7 @@ import sys
 import types
 
 __author__ = "Benjamin Peterson <benjamin@python.org>"
-__version__ = "1.8.0"
+__version__ = "1.9.0"
 
 
 # Useful for very coarse version differentiation.
