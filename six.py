a41bf1f9e8f8d703181e8373d4ed4db24dd8fe93
Max Grender-Jones <MaxGrenderJones@gmail.com>
2015-01-03 10:49:20 +0000

    Fixes #108 (os.getcwd has been renamed in move from 2 to 3)

--- six.py
+++ six.py
@@ -234,6 +234,8 @@ _moved_attributes = [
     MovedAttribute("input", "__builtin__", "builtins", "raw_input", "input"),
     MovedAttribute("intern", "__builtin__", "sys"),
     MovedAttribute("map", "itertools", "builtins", "imap", "map"),
+    MovedAttribute("getcwd", "os", "os", "getcwdu", "getcwd"),
+    MovedAttribute("getcwdb", "os", "os", "getcwd", "getcwdb"),
     MovedAttribute("range", "__builtin__", "builtins", "xrange", "range"),
     MovedAttribute("reload_module", "__builtin__", "imp", "reload"),
     MovedAttribute("reduce", "__builtin__", "functools"),
