c7909fab088b97ced9ea153e6b9d324c9e3a9c43
Bart van Merrienboer <bart.vanmerrienboer@gmail.com>
2015-01-21 12:51:23 -0500

    Added tests

--- test_six.py
+++ test_six.py
@@ -456,6 +456,20 @@ def test_create_bound_method():
     assert b() is x
 
 
+def test_create_unbound_method():
+    class X(object):
+        pass
+
+    def f(self):
+        return self
+    u = six.create_unbound_method(f, X)
+    py.test.raises(TypeError, u)
+    if six.PY2:
+        assert isinstance(u, types.MethodType)
+    x = X()
+    assert f(x) is x
+
+
 if six.PY3:
 
     def test_b():
