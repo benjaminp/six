c3ec058bf8c4d6329224eac53366c8e9ce511417
Benjamin Peterson <benjamin@python.org>
2015-10-06 20:13:10 -0700

    bump version to 1.10.0

--- six.py
+++ six.py
@@ -29,7 +29,7 @@ import sys
 import types
 
 __author__ = "Benjamin Peterson <benjamin@python.org>"
-__version__ = "1.9.0"
+__version__ = "1.10.0"
 
 
 # Useful for very coarse version differentiation.
