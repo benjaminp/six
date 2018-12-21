9618916a066cb39e80673c475f4a59859b8d42c1
Jon Dufresne <jon.dufresne@gmail.com>
2018-12-20 19:47:44 -0800

    Remove distutils fallback and outdated comment
    setuptools includes a vendored version of six (and other dependencies).
    They are not installed through traditional tools. Therefore, distutils
    is not required as a fallback to facilitate setuptools.
    
    https://github.com/pypa/setuptools/blob/v40.6.3/setuptools/_vendor/six.py

--- setup.py
+++ setup.py
@@ -20,13 +20,7 @@
 
 from __future__ import with_statement
 
-# Six is a dependency of setuptools, so using setuptools creates a
-# circular dependency when building a Python stack from source. We
-# therefore allow falling back to distutils to install six.
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 import six
 
