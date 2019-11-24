db8eb193994f90c0526c02b8f387d57507e99ee7
Hugo van Kemenade <hugovk@users.noreply.github.com>
2019-11-24 12:42:23 +0200

    Add 3.8

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ six_classifiers = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: PyPy",
     "Intended Audience :: Developers",
