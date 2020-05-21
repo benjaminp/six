c0be8815d13df45b6ae471c4c436cce8c192245d
Benjamin Peterson <benjamin@python.org>
2020-05-21 10:19:37 -0500

    Advance version to 1.15.0.

--- six.py
+++ six.py
@@ -29,7 +29,7 @@ import sys
 import types
 
 __author__ = "Benjamin Peterson <benjamin@python.org>"
-__version__ = "1.14.0"
+__version__ = "1.15.0"
 
 
 # Useful for very coarse version differentiation.
